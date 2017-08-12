44e5561f783a5ed5d214013773ca73bea0ef7dd0
Michael <michael@crispcode.de>
2017-08-12 10:20:55 +0200

    adjusted tutorials

--- tutorial/helloconfig/widget.js
+++ tutorial/helloconfig/widget.js
@@ -1,35 +1,43 @@
+// EveryAware provides jquery 3.2.1 when requiring 'jquery'
 define(['jquery'], function($) {
 
 	/**
  	 * This widget shows the string "Hello <name>!". Where <name> is set via the config.
  	 */
 	function Widget(selector, config) {
-		
+
 		// save selector for later reference
 		this.selector = selector;
-		
+
 		// set name to print from config or use default
 		if (config && config.name) {
 			this.name = config.name;
 		} else {
 			this.name = "Config"
 		}
-		
+
 		// render content
 		this.renderHelloName();
 	}
 
 	/**
 	 * Custom function to draw the "Hello <name>!" string.
-         */	
+   */
 	Widget.prototype.renderHelloName = function() {
 		$(this.selector).html('Hello ' + this.name + '!');
 	}
-	
-	Widget.prototype.getConfig = function() { 
-		return { name: this.name }; 
+
+	/**
+	 * Mandatory functions
+	 */
+	Widget.prototype.getConfig = function() {
+		return { name: this.name };
 	}
-	
+
+	Widget.prototype.getSelector = function() {
+		return this.selector;
+	}
+
 	Widget.prototype.renderConfigDialog = function(selector, callback) {
 		var self = this
 		$(selector).html(
@@ -37,18 +45,20 @@ define(['jquery'], function($) {
 		$(selector).find('button').on('click', function() {
 			self.name = $("input", selector).val();
 			self.renderHelloName();
+			// The callback needs to be executed to tell the dashboard that the config
+			// dialog should be closed now
 			callback();
 		});
 	};
-	
+
 	/*
-	 * These are some required life cycle functions. 
+	 * These are some required life cycle functions.
 	 * We don't need to implement anything more sophisticated for this simple widget.
 	 * Other tutorials go into more detail.
 	 */
 	Widget.prototype.redraw = function() {};
 	Widget.prototype.destroy = function() {};
-	
+
 	// return our widget
 	return Widget;
 
--- tutorial/helloworld/widget.js
+++ tutorial/helloworld/widget.js
@@ -1,26 +1,28 @@
-// TODO: add jquery version number on EveryAware
-// TODO: references to more advanced tutorials to explain life cycle methods
-// TODO: test if this works at all :D
+// EveryAware provides jquery 3.2.1 when requiring 'jquery'
 define(['jquery'], function($) {
-	
+
 	/**
  	 * This widget shows the string "Hello World!".
  	 */
 	function Widget(selector, config) {
+		// save selector for later reference
+		this.selector = selector;
+
 		$(selector).html("Hello World!");
 	}
-	
+
 	/*
-	 * These are some required life cycle functions. 
+	 * These are some required life cycle functions.
 	 * We don't need to implement anything more sophisticated for this simple widget.
 	 * Other tutorials go into more detail.
 	 */
+	Widget.prototype.getConfig = function() { return {}; }
+	Widget.prototype.getSelector = function() {return this.selector;}
+	Widget.prototype.renderConfigDialog = function(selector, callback) {};
 	Widget.prototype.redraw = function() {};
 	Widget.prototype.destroy = function() {};
-	Widget.prototype.getConfig = function() { return {}; }
-	Widget.prototype.renderConfigDialog = function(selector, callback) {};
-	
+
 	// return our widget
 	return Widget;
-	
+
 });
