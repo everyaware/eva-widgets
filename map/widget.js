99d6be8d9e8d372c819ae720f17d163a4333ae02
Michael Steininger <michaelsteininger@win6095.informatik.uni-wuerzburg.de>
2018-08-28 17:03:44 +0200

    coolere maps

--- map/widget.js
+++ map/widget.js
@@ -1,6 +1,36 @@
 define(['jquery', 'leaflet', 'app/config-view', 'css!./widget', 'css!leaflet-css'],
     function ($, L, ConfigView) {
 
+
+    L.TileLayer.WMS.include({
+        // getLegendGraphic returns a hash of legend URLs. The keys are the layer names the values are the legend URLs
+        getLegendGraphic: function(options){
+
+            var layers = this.wmsParams.layers.split(',');
+            var legends = {};
+
+            var legendReq = {
+                'SERVICE' : 'WMS',
+                'LAYER'   : this.wmsParams.layers,
+                'REQUEST' : 'GetLegendGraphic',
+                'VERSION' : this.wmsParams.version,
+                'FORMAT'  : this.wmsParams.format,
+                'WIDTH'   : 20,
+                'HEIGHT'  : 20
+            };
+
+            legendReq = L.extend(legendReq,options);
+            var url = L.Util.template(this._url)
+
+            for(var i = 0;i<layers.length;i++){
+                legendReq.LAYER = layers[i];
+                legends[layers[i]] = url + L.Util.getParamString(legendReq, url, true);
+            }
+
+            return legends;
+        }
+    });
+
 	/**
 	 * This widget is for displaying a map.
 	 */
@@ -67,10 +97,12 @@ define(['jquery', 'leaflet', 'app/config-view', 'css!./widget', 'css!leaflet-css
         }).addTo(this.map);
 
         for (var tileServerIndex in this.config.wmsTileServers) {
-            L.tileLayer.wms(this.config.wmsTileServers[tileServerIndex].url, {
+            var layer = L.tileLayer.wms(this.config.wmsTileServers[tileServerIndex].url, {
                 layers: this.config.wmsTileServers[tileServerIndex].layers.join(','),
                 opacity: this.config.wmsTileServers[tileServerIndex].opacity
-            }).addTo(this.map);
+            });
+            console.log(layer.getLegendGraphic());
+            layer.addTo(this.map);
         }
     }
 
